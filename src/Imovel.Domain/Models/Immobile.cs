664c3d01bbfa08792a1dd5d2c5ae0b7eb1595379
italo.leite <italo.leite@nibo.com.br>
2021-09-17 22:45:14 -0300

    criando classe imóvel e tipo

---
+++ src/Imovel.Domain/Models/Immobile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imovel.Domain.Models
+{
+    public class Immobile
+    {
+        public Guid ImmobileId { get; set; }
+        public ImmobileType ImmobileType { get; set; }
+
+        public Immobile(ImmobileType immobileType)
+        {
+            
+            ImmobileId = Guid.NewGuid();
+            ImmobileType = immobileType;
+            Validar(immobileType);
+        }
+
+        private void Validar(ImmobileType immobileType)
+        {
+            if (immobileType == ImmobileType.UNDEFINED) throw new Exception("Deve ser definido um tipo de imóvel");
+        }
+    }
+}
---
+++ src/Imovel.Domain/Models/ImmobileType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imovel.Domain.Models
+{
+    public enum ImmobileType
+    {
+        UNDEFINED,
+        RESIDENTIAL,
+        COMMERCIAL
+
+    }
+}
