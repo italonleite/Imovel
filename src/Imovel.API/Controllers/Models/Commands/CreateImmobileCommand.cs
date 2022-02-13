5102842ccf9eeb8542b4173233655c93f5b16293
italo.leite <italo.leite@nibo.com.br>
2022-02-12 22:32:36 -0300

    teste push

--- src/Imovel.API/Controllers/Models/Commands/CreateImmobileCommand.cs
+++ src/Imovel.API/Controllers/Models/Commands/CreateImmobileCommand.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Imovel.API.Controllers.Models.Commands
 {
+    //teste
     public class CreateImmobileCommand
     {
         public ImmobileType ImmobileType { get; set; }
